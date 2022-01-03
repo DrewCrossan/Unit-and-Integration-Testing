3737a931fd301fe4892c5dee97dc143c79e8e5c4
Andrew Crossan <andrewcrossanit@gmail.com>
2022-01-03 23:12:33 +0000

    initial commit for pda

---
+++ cypress/integration/calculator.spec.js
@@ -0,0 +1,74 @@
+describe("Calculator", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  })
+
+  it('should have working number buttons', () => {
+    cy.get('#number2').click();
+    cy.get('.display').should('contain', '2')
+  })
+
+  it('should update the display of the running total when number buttons are pressed', () => {
+    cy.get('#number3').click();
+    cy.get('#number4').click();
+    cy.get('#number5').click();
+    cy.get('.display').should('contain', '345')
+  })
+
+  it('should update the display with the result of an arithmetical operation', () => {
+    cy.get('#number3').click();
+    cy.get('#operator_add').click();
+    cy.get('#number6').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '9')
+  })
+
+  it('should be able to chain multiple operations together', () => {
+    cy.get('#number3').click();
+    cy.get('#operator_add').click();
+    cy.get('#number6').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number3').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number9').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '27')
+  })
+
+  it('output should be as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number9').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-6')
+    cy.get('#operator_add').click();
+    cy.get('#number9').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '3')
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '1.5')
+    cy.get('#operator-multiply').click();
+    cy.get('#number1').click();
+    cy.get('#number2').click();
+    cy.get('#number3').click();
+    cy.get('#number4').click();
+    cy.get('#number5').click();
+    cy.get('#number6').click();
+    cy.get('#number7').click();
+    cy.get('#number8').click();
+    cy.get('#number9').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '185185183.5')
+  })
+
+  it("should tell user they can't divide by 0", () => {
+    cy.get('#number1').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', "Can't divide by zero")
+  })
+
+})
